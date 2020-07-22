63f8caa08aab2afb033877b5e4f3758a9193a90c
flatsiedatsie <unfold@gmail.com>
2020-07-22 19:58:44 +0200

    0.1.2
    Avoid copying hidden files

--- package.sh
+++ package.sh
@@ -6,11 +6,13 @@ rm -rf SHA256SUMS package
 rm -rf ._*
 mkdir package
 
-cp -r pkg LICENSE manifest.json *.py README.md package/
+cp -r pkg LICENSE manifest.json *.py README.md css images js views package/
 find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
 find package -type d -empty -delete
+sudo rm -rf /home/pi/.mozilla-iot/addons/followers/package/pkg/pycache
+#cp -r pkg css images js views package/
 
-cp -r pkg css images js views package/
 cd package
 find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
 cd ..
